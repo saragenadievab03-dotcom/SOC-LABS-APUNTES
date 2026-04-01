d27c4f35ecec9b05bde205ed076a1cef5c70f8bf
cybersara <saragenadievab03@gmail.com>
2026-04-01 18:51:05 +0200

    python-basico-1: diccionario_recetas
    Script básico de Python para practicar diccionarios y estructuras de datos

---
+++ python/python-basic/diccionario_recetas.py
@@ -0,0 +1,21 @@
+# diccionario_recetas.py
+
+recetas = {
+    "tarta": ["harina", "huevos", "azúcar"],
+    "pizza": ["masa", "tomate", "queso"],
+    "ensalada": ["lechuga", "tomate", "aceite"]
+}
+
+for r in recetas:
+    print(r, ":", recetas[r])
+  
+
+# resultado
+tarta : ['harina', 'huevos', 'azúcar']
+pizza : ['masa', 'tomate', 'queso']
+ensalada : ['lechuga', 'tomate', 'aceite']
+
+
+# aprendizaje:
+# - Acceder a datos con su clave
+# - Ver listas dentro de un diccionario
